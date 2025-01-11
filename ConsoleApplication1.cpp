d3722451fca287ff1041f6045b2e06f3baec654d
michalmatysiak2004 <mmatysiak2004@gmail.com>
2025-01-11 14:58:56 +0100

    6pkt

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -25,101 +25,117 @@ struct Data { // struct danych
 };
 
 void print_metar(struct Data* data) {
-    
+    for (int i = 0; i < 3; ++i) {
+        if (data[i].airport_code[0] == 'E') {
+            cout << "GDANSK" << endl;
+        }
+        cout << "Dzien: " << data[i].day << " godzina: " << data[i].hour
+            << ":" << (data[i].minutes < 10 ? "0" : "") << data[i].minutes << endl;
+        cout << "Wiatr: " << data[i].angle << " stopni, " << data[i].velocity
+            << " wezlow" << endl;
+        cout << "Temperatura: " << data[i].temperature << "°C" << endl;
+        cout << "Temperatura punktu rosy: " << data[i].rose_temperature << "°C" << endl;
+        cout << "-------------------------------------" << endl;
+    }
 }
-void switchlastmetars(struct Data * data, struct Data current) {
-    
+
+void switchlastmetars(struct Data * lasts, struct Data current,int counter) { 
+            lasts[2] = lasts[1];
+            lasts[1] = lasts[0];
+            lasts[0] = current;
+   
 }
-void save_metar(struct Data* data, ofstream& file) {
-    
-    
-    if (data->airport_code[0] == 'E') {
+
+
+void save_metar(struct Data data, ofstream& file) {
+    if (data.airport_code[0] == 'E') {
         file << "GDANSK" << endl;
     }
-    
-    file << "Dzien: " << data->day << "godzina: " << data->hour << ":"
-        << data->minutes << endl;
-    file << "Wiatr: " << data->angle << " stoopni, " << data->velocity << " wezlow" << endl;
-    file << "Temperature: " << data->temperature << "°C" << endl;
-
-
-
-
-    file << "Rose Temperature: " << data->rose_temperature << "°C" << endl;
+    file << "Dzien: " << data.day << "godzina: " << data.hour << ":"
+        << data.minutes << endl;
+    file << "Wiatr: " << data.angle << " stoopni, " << data.velocity << " wezlow" << endl;
+    file << "Temperature: " << data.temperature << "°C" << endl;
+    file << "Rose Temperature: " << data.rose_temperature << "°C" << endl;
 };
 
-void handle_word(const char* word, struct  Data* data) {
-    if (data->airport_code[0] == 0) {
+struct Data handle_word(const char* word, struct  Data data) {
+    if (data.airport_code[0] == 0) {
         for (int i = 0; i < 4; i++) {
-            data->airport_code[i] = word[i];
+            data.airport_code[i] = word[i];
         }
     }
-    else if (data->day == 0) {
-        char day_str[3] = { word[0], word[1], '\0' }; // Dzień: pierwsze dwa znaki
-        char hour_str[3] = { word[2], word[3], '\0' }; // Godzina: kolejne dwa znaki
-        char minutes_str[3] = { word[4], word[5], '\0' }; // Minuty: następne dwa znaki
+    else if (data.day == 0) {
+        char day_str[3] = { word[0], word[1], '\0' }; 
+        char hour_str[3] = { word[2], word[3], '\0' }; 
+        char minutes_str[3] = { word[4], word[5], '\0' }; 
 
-        data->day = std::atoi(day_str);      // Konwersja na liczbę całkowitą
-        data->hour = std::atoi(hour_str);    // Konwersja na liczbę całkowitą
-        data->minutes = std::atoi(minutes_str); // Konwersja na liczbę całkowitą
+        data.day = atoi(day_str);      
+        data.hour = atoi(hour_str);   
+        data.minutes = atoi(minutes_str);
     }
     else if (word[strlen(word)-2] == 'K' && word[strlen(word)-1] == 'T') {
         
-        // Sprawdź, czy na końcu jest "KT"
-        // Zakładając, że word zawiera kierunek i prędkość (np. "25009KT")
-        char angle_str[4] = { word[0], word[1], word[2], '\0' }; // Kierunek: pierwsze trzy znaki
-        char vel_str[3] = { word[3], word[4], '\0' };        // Prędkość: kolejne dwa znaki
+      
+        char angle_str[4] = { word[0], word[1], word[2], '\0' }; 
+        char vel_str[3] = { word[3], word[4], '\0' };       
 
-        data->angle = std::atoi(angle_str);  // Konwersja na liczbę całkowitą (kierunek wiatru)
-        data->velocity= std::atoi(vel_str);     // Konwersja na liczbę całkowitą (prędkość wiatru)
+        data.angle = atoi(angle_str); 
+        data.velocity= atoi(vel_str);    
     }
-    else if (word[2] == '/') {  // Przetwarzanie formatu temperatury punktu rosy
-        // Zakładając, że word jest w formacie "03/02" gdzie 03 to temperatura, a 02 to temperatura punktu rosy
-        char temp_str[3] = { word[0], word[1], '\0' };  // Temperatura: pierwsze dwa znaki
-        char rose_temp_str[3] = { word[3], word[4], '\0' };  // Temperatura punktu rosy: kolejne dwa znaki
+    else if (word[2] == '/') { 
+        
+        char temp_str[3] = { word[0], word[1], '\0' };  
+        char rose_temp_str[3] = { word[3], word[4], '\0' };  
 
-        data->temperature = std::atoi(temp_str);  // Konwersja na temperaturę
-        data->rose_temperature = std::atoi(rose_temp_str);  // Konwersja na temperaturę punktu rosy
+        data.temperature = atoi(temp_str);  
+        data.rose_temperature = atoi(rose_temp_str);  
     }
         
-    
+    return data;
 }
 
 void program() {
-    std::ifstream file;
+    ifstream file;
     file.open("dane.txt");
-    std::ofstream file2;
-    file2.open("wyniki.txt", std::ios::app); // Otwórz plik w trybie dopisywania
-    Data lastmetars[3];
+    ofstream file2;
+    file2.open("wyniki.txt", ios::app); 
+
+
+    Data lastmetars[3]; // tablica 3 ostatnich metarów
+    Data data;
+
+
+
     if (!file.is_open()) {
-        std::cerr << "Nie mozna otworzyc pliku dane.txt" << std::endl;
+        cerr << "Nie mozna otworzyc pliku dane.txt" << endl;
         return; // Zakończ funkcję, jeśli nie można otworzyć pliku
     }
     if (!file2.is_open()) {
-        std::cerr << "Nie mozna otworzyc pliku wyniki.txt" << std::endl;
+        cerr << "Nie mozna otworzyc pliku wyniki.txt" << endl;
         file.close();
         return; // Zakończ funkcję, jeśli nie można otworzyć pliku wynikowego
     }
 
     char single_metar[256]; // Bufor do przechowywania linii z pliku
+    int counter = 0;
     while (file.getline(single_metar, sizeof(single_metar))) {
-        Data* data = new Data;
+        
         char* context = nullptr; // Kontekst wymagany przez strtok_s
         char* token = strtok_s(single_metar, " ", &context);
         while (token != nullptr) {
-            handle_word(token, data); // Przekazujemy wyraz do funkcji handle_word
+            data = handle_word(token, data); // Przekazujemy wyraz do funkcji handle_word
             token = strtok_s(nullptr, " ", &context);
         }
-
-        switchlastmetars(lastmetars,);
-        print_metar(data);
+        
+        switchlastmetars(lastmetars,data,counter);
+        counter++;
+        print_metar(lastmetars);
 
         
-        std::cout << "Nacisnij Enter, aby zapisac ten METAR do pliku..." << std::endl;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Czekaj na Enter
+        cout << "Nacisnij Enter, aby zapisac ten METAR do pliku..." << endl;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Czekaj na Enter
 
         save_metar(data, file2); // Zapisz pojedynczy METAR do pliku
-        delete data;
         cout << endl;
     }
     file.close();
